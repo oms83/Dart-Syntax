67842c6ac5547ee813bcd7c7477fa34777831569
oms83 <omer.memes20@gmail.com>
2023-10-25 08:24:00 +0300

    Constructors Type

--- class.dart
+++ class.dart
@@ -5,7 +5,22 @@ class clsA {
   double? _Salary;
   double? _Tax;
 
-  clsA(this._Age, this._Name, this._Surname, this._Salary, this._Tax);
+  clsA(this._Age, this._Name, this._Surname, this._Salary, [this._Tax = 5]);
+  clsA.Named(this._Age, this._Name, this._Surname, this._Salary,
+      [this._Tax = 10]);
+
+  clsA.Named2(
+      {int? Age,
+      String? Name,
+      String? Surname,
+      double? Tax = 15,
+      double? Salary}) {
+    this._Age = Age;
+    this._Name = Name;
+    this._Surname = Surname;
+    this._Tax = Tax;
+    this._Salary = Salary;
+  }
 
   int get Age => _Age!;
   double get Salary => _Salary!;
@@ -29,11 +44,18 @@ class clsA {
     print("Age              : ${this._Age}");
     print("Salary           : ${this._Salary}");
     print("Tax              : ${this._Tax}");
-    print("Salary After Tax : ${SalaryAfterTax()}");
+    print("Salary After Tax : ${SalaryAfterTax()}\n\n");
   }
 }
 
 main() {
-  clsA obA = new clsA(23, "Omer", "Memes", 3000, 5);
+  clsA obA = new clsA(23, "Omer", "Memes", 3000);
   obA.Display();
+
+  clsA obB = new clsA.Named(23, "Omer", "Memes", 3000);
+  obB.Display();
+
+  clsA obC =
+      new clsA.Named2(Age: 23, Name: "Omer", Surname: "Memes", Salary: 3000);
+  obC.Display();
 }
